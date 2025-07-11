70700829a7ba85d603cdc6a50295f86c70a4bfde
Matthew Hipp <hipp.matthew@icloud.com>
2025-07-10 23:30:49 -0400

    fractals!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "groot/enums.hpp"
+#include "groot/material_manager.hpp"
 #include <groot/groot.hpp>
 
 int main() {
@@ -19,12 +21,24 @@ int main() {
     .add_canvas()
   );
 
+  engine.add_material("julia", ge::MaterialManager::Builder()
+    .add_shader(ge::ShaderStage::VertexShader, "shaders/quad.vert.spv")
+    .add_shader(ge::ShaderStage::FragmentShader, "shaders/quad.frag.spv")
+    .add_shader(ge::ShaderStage::ComputeShader, "shaders/julia.comp.spv")
+    .compute_space(128, 128)
+    .add_canvas()
+  );
+
   static_cast<void>(engine.add_object("mandelbrot", "../obj/quad.obj",
-    ge::Transform(ge::vec3(-1.0, 0.0, 0.0), ge::vec3(0.0), ge::vec3(1.0))
+    ge::Transform(ge::vec3(-1.0, -0.5, 0.0), ge::vec3(0.0), ge::vec3(0.5))
   ));
 
   static_cast<void>(engine.add_object("newton", "../obj/quad.obj",
-    ge::Transform(ge::vec3(1.0, 0.0, 0.0), ge::vec3(0.0), ge::vec3(1.0))
+    ge::Transform(ge::vec3(1.0, -0.5, 0.0), ge::vec3(0.0), ge::vec3(0.5))
+  ));
+
+  static_cast<void>(engine.add_object("julia", "../obj/quad.obj",
+    ge::Transform(ge::vec3(0.0, 0.5, 0.0), ge::vec3(0.0), ge::vec3(0.5))
   ));
 
   engine.run();
